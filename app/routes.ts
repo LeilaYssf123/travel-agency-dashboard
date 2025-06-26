c021e6b10f06b7cf5a287d5374a63b0d8b5b1f21
Leila <leila.yousfi@esprit.tn>
2025-06-26 17:39:25 +0100

    test

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import {type RouteConfig, index, route, layout} from "@react-router/dev/routes";
 
 export default [
     route('sign-in','routes/root/sign-in.tsx'),
-
+    route('api/create-trip', 'routes/api/create-trip.ts'),
     layout("routes/Admin/Admin-layout.tsx",[
     route('Dashboard','routes/Admin/Dashboard.tsx'),
         route('All-users','routes/Admin/All-users.tsx'),
